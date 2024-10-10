0978f736a870584f944f1040014e072c7136b290
Etheldra Akahoho <etheldeak4@gmail.com>
2024-10-10 11:39:42 +0000

    Worked in the editBook page

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import { Link } from 'react-router-dom';
 import Footer from './components/Footer';
-import libraryVideo from '/public/videos/libvideo.mp4'
+import libraryVideo from '/public/videos/libvideo.mp4';
 
 const LandingPage = () => {
   const [showSplash, setShowSplash] = useState(true);
